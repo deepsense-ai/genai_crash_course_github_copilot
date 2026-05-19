2083f523cd545f5ea509d94deeb5c85ff429118c
Bartosz Mikulski <bartosz.mikulski@deepsense.ai>
2026-05-19 16:29:47 +0200

    Redesign Chapter 1

--- chapter2/ex1.py
+++
@@ -1,5 +0,0 @@
-# Task:
-# Write a Python script that based on user location plots the forcasted temperature 
-# for the next 7 days. You should use https://open-meteo.com/ API to get the weather data.
-# You can use any plotting library you like and save the plot as an image file.
-
